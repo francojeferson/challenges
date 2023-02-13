dfc958c8aa76df34e93160ed7583a500a9c60023
jeferson-franco <jefersonfranco@pm.me>
2023-02-13 14:35:16 -0300

    feat: create main3.js

---
+++ javascript/codesignal/adjacentElementsProduct/main3.js
@@ -0,0 +1,11 @@
+function solution(inputArray) {
+    var prod = inputArray[0] * inputArray[1];
+
+    for (var i = 1; i < inputArray.length - 1; i++) {
+        prod = Math.max(prod, inputArray[i] * inputArray[i + 1]);
+    }
+
+    return prod
+}
+
+module.exports = solution;
