c03d4747f26c2f7f578170c68e654ab7e81e849a
jeferson-franco <jefersonfranco@pm.me>
2023-02-13 17:43:55 -0300

    feat: create main2.js

---
+++ javascript/codesignal/reverseInParentheses/main2.js
@@ -0,0 +1,8 @@
+function solution(inputString) {
+    while (inputString.includes('(')) {
+        inputString = inputString.replace(/\(([^()]*)\)/, (_, str) => [...str].reverse().join(''));
+    }
+    return inputString;
+}
+
+module.exports = solution;
