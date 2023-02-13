5ab98427a866cc2e76e8f14fa96d75b02bc25322
jeferson-franco <jefersonfranco@pm.me>
2023-02-13 15:17:14 -0300

    update main3.js

--- javascript/codesignal/adjacentElementsProduct/main3.js
+++ javascript/codesignal/adjacentElementsProduct/main3.js
@@ -1,7 +1,7 @@
 function solution(inputArray) {
-    var prod = inputArray[0] * inputArray[1];
+    let prod = inputArray[0] * inputArray[1];
 
-    for (var i = 1; i < inputArray.length - 1; i++) {
+    for (let i = 1; i < inputArray.length - 1; i++) {
         prod = Math.max(prod, inputArray[i] * inputArray[i + 1]);
     }
 
