01585b346fbaceaa7633f32556f07c54fc2203da
jeferson-franco <jefersonfranco@pm.me>
2023-02-14 11:20:33 -0300

    feat: create main.js

---
+++ javascript/codesignal/palindromeRearranging/main.js
@@ -0,0 +1,19 @@
+function solution(inputString) {
+    const charCount = {};
+    for (let i = 0; i < inputString.length; i++) {
+        if (charCount[inputString[i]]) {
+            charCount[inputString[i]]++;
+        } else {
+            charCount[inputString[i]] = 1;
+        }
+    }
+    let oddCount = 0;
+    for (let key in charCount) {
+        if (charCount[key] % 2 === 1) {
+            oddCount++;
+        }
+    }
+    return oddCount <= 1;
+}
+
+module.exports = solution;
