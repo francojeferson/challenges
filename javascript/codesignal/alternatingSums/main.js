765f315f5788a612083f87e37929ebf01142169b
jeferson-franco <jefersonfranco@pm.me>
2023-02-14 09:56:56 -0300

    feat: create main.js

---
+++ javascript/codesignal/alternatingSums/main.js
@@ -0,0 +1,14 @@
+function solution(a) {
+    let even = 0;
+    let odd = 0;
+    for (const val of a.entries()) {
+        if (val[0] % 2 === 0) {
+            even += val[1];
+        } else {
+            odd += val[1];
+        }
+    }
+    return [even, odd];
+}
+
+module.exports = solution;
