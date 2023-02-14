40d11ca138c712cda89a74bb6cc0201a7d3716c3
jeferson-franco <jefersonfranco@pm.me>
2023-02-14 10:23:38 -0300

    feat: create main2.js

---
+++ javascript/codesignal/addBorder/main2.js
@@ -0,0 +1,10 @@
+function solution(picture) {
+    let width = picture[0].length + 2;
+    return [
+        '*'.repeat(width),
+        ...picture.map(line => `*${line}*`),
+        '*'.repeat(width)
+    ];
+}
+
+module.exports = solution;
