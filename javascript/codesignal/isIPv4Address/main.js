4acea4a1b51c1c04beeb89e5d50634398e267d34
jeferson-franco <jefersonfranco@pm.me>
2023-02-15 14:03:33 -0300

    feat: create main.js

---
+++ javascript/codesignal/isIPv4Address/main.js
@@ -0,0 +1,6 @@
+function solution(inputString) {
+    const regex = /^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/;
+    return regex.test(inputString);
+}
+
+module.exports = solution
