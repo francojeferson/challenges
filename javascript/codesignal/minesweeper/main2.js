a2852d4128c1fac61b6338c2ed6cef9686019cf2
jeferson-franco <jefersonfranco@pm.me>
2023-02-15 14:54:21 -0300

    feat: create main2.js

---
+++ javascript/codesignal/minesweeper/main2.js
@@ -0,0 +1,9 @@
+const directions = [
+    [1, -1], [1, 0], [1, 1],
+    [0, -1], [0, 1],
+    [-1, -1], [-1, 0], [-1, 1]
+];
+
+solution = matrix => matrix.map((row, y) => row.map((col, x) => directions.reduce((count, i) => count += !!(matrix[y + i[0]] && matrix[y + i[0]][x + i[1]]), 0)));
+
+module.exports = solution;
