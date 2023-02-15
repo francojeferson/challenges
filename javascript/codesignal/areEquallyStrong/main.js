73b916084367193b4231b6b0ad11e7ce1997c18d
jeferson-franco <jefersonfranco@pm.me>
2023-02-15 11:59:18 -0300

    feat: create main.js

---
+++ javascript/codesignal/areEquallyStrong/main.js
@@ -0,0 +1,5 @@
+function solution(yourLeft, yourRight, friendsLeft, friendsRight) {
+    return (yourLeft === friendsLeft && yourRight === friendsRight) || (yourLeft === friendsRight && yourRight === friendsLeft);
+}
+
+module.exports = solution;
