fe8be9f9d51daa952155382cfa8d10d31a27819c
jeferson-franco <jefersonfranco@pm.me>
2023-02-15 12:08:57 -0300

    feat: create main.js

---
+++ javascript/codesignal/arrayMaximalAdjacentDifference/main.js
@@ -0,0 +1,12 @@
+function solution(inputArray) {
+    let maxDiff = 0;
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        let diff = Math.abs(inputArray[i] - inputArray[i + 1]);
+        if (diff > maxDiff) {
+            maxDiff = diff;
+        }
+    }
+    return maxDiff;
+}
+
+module.exports = solution;
