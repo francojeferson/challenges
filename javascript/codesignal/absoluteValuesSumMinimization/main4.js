be9502689b002eb0661be33531406045b575e928
jeferson-franco <jefersonfranco@pm.me>
2023-02-24 19:23:08 -0300

    chore: delete main4.js

--- javascript/codesignal/absoluteValuesSumMinimization/main4.js
+++
@@ -1,6 +0,0 @@
-const solution = a => {
-    let distances = a.map(e => a.reduce((acc, cur) => acc + Math.abs(cur - e), 0));
-    return a[distances.indexOf(Math.min(...distances))];
-};
-
-module.exports = solution;
