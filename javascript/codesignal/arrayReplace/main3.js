6a62d8270a6a912d718eb9eb3688524411dcbf48
jeferson-franco <jefersonfranco@pm.me>
2023-02-24 21:16:54 -0300

    chore: delete main3.js

--- javascript/codesignal/arrayReplace/main3.js
+++
@@ -1,9 +0,0 @@
-function solution(inputArray, elemToReplace, substitutionElem) {
-    for (let i = 0; i < inputArray.length; i++) {
-        if (inputArray[i] == elemToReplace)
-            inputArray[i] = substitutionElem
-    }
-    return inputArray
-}
-
-module.exports = solution
