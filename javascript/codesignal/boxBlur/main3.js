b43a9aad251ec813be77f9885df082e273f0c7e9
jeferson-franco <jefersonfranco@pm.me>
2023-02-27 11:29:06 -0300

    chore: delete main3.js

--- javascript/codesignal/boxBlur/main3.js
+++
@@ -1,4 +0,0 @@
-const solution = image =>
-    image.slice(1, -1).map(val => val.slice(1, -1)).map((val, i) => val.map((_, j) => [...Array(3).keys()].reduce((pre, x, _, arr) => pre + arr.reduce((pre, y) => pre + image[i + x][j + y], 0), 0) / 9 ^ 0));
-
-module.exports = solution;
