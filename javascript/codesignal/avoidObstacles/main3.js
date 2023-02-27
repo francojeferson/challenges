0ac9d8bf6d6cf5e4631d16c012d0330962f46be8
jeferson-franco <jefersonfranco@pm.me>
2023-02-27 11:43:29 -0300

    chore: delete main3.js

--- javascript/codesignal/avoidObstacles/main3.js
+++
@@ -1,9 +0,0 @@
-function solution(inputArray) {
-    let jump = 2;
-    while (inputArray.some((obstacle) => obstacle % jump == 0)) {
-        jump++;
-    }
-    return jump;
-}
-
-module.exports = solution
