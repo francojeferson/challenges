43a1512f71b856e6a7c58f4f09c767d33be5189f
jeferson-franco <jefersonfranco@pm.me>
2023-02-27 14:26:50 -0300

    chore: delete main3.js

--- javascript/codesignal/areSimilar/main3.js
+++
@@ -1,6 +0,0 @@
-function solution(A, B) {
-    for (let r = [], i = 0; i < A.length; i++) if (A[i] != B[i]) r.push(A[i], B[i])
-    return r.length < 5 && new Set(r).size < 3
-}
-
-module.exports = solution;
