fe24bc81abdb75ad3bd15dc7cfd3c5e93db56544
jeferson-franco <jefersonfranco@pm.me>
2023-02-27 12:56:26 -0300

    chore: delete main3.js

--- javascript/codesignal/arrayChange/main3.js
+++
@@ -1,8 +0,0 @@
-function solution(inputArray) {
-    return inputArray.reduce(function (x, b, i, a) {
-        while (b >= a[i + 1]) { x++; a[i + 1]++; };
-        return x;
-    }, 0)
-}
-
-module.exports = solution;
