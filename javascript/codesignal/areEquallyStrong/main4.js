b2e50e6a4fd8ffd689f5d947fc2240e6f3080150
jeferson-franco <jefersonfranco@pm.me>
2023-02-27 12:18:24 -0300

    chore: delete main4.js

--- javascript/codesignal/areEquallyStrong/main4.js
+++
@@ -1,5 +0,0 @@
-function solution(yourLeft, yourRight, friendsLeft, friendsRight) {
-    return [yourLeft, yourRight].filter(x => x == friendsLeft || x == friendsRight).length == 2
-}
-
-module.exports = solution
