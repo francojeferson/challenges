1bdb4154619309f9d87a4ee58e4af65778de8290
jeferson-franco <jefersonfranco@pm.me>
2023-02-27 12:18:40 -0300

    chore: delete main3.js

--- javascript/codesignal/areEquallyStrong/main3.js
+++
@@ -1,5 +0,0 @@
-function solution(yourLeft, yourRight, friendsLeft, friendsRight) {
-    return Math.max(yourLeft, yourRight) === Math.max(friendsLeft, friendsRight) && Math.min(yourLeft, yourRight) === Math.min(friendsLeft, friendsRight)
-}
-
-module.exports = solution
