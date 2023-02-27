abb630f92563a7305b86401e98a3a59351dd9284
jeferson-franco <jefersonfranco@pm.me>
2023-02-27 14:26:37 -0300

    chore: delete main4.js

--- javascript/codesignal/areSimilar/main4.js
+++
@@ -1,8 +0,0 @@
-function solution(a, b) {
-    sum = 0;
-    for (i = 0; i < a.length; i++) { if (a[i] != b[i]) { sum++ } }
-    a.sort(); b.sort();
-    return a.toString() == b.toString() && sum < 3
-}
-
-module.exports = solution;
