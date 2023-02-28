d7c75f6805cce879ebabbde6be801ee6825a08aa
jeferson-franco <jefersonfranco@pm.me>
2023-02-28 20:55:13 -0300

    chore: delete main3.js

--- javascript/codesignal/sortByHeight/main3.js
+++
@@ -1,7 +0,0 @@
-function solution(a) {
-    let filtered = a.filter(val => val !== -1)
-    let sorted = filtered.sort((a, b) => a - b)
-    return a.map(val => val === -1 ? -1 : sorted.shift())
-}
-
-module.exports = solution;
