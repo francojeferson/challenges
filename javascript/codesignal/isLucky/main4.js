8e4ef65c20f8645757283b4f64e98dfc4a494ddc
jeferson-franco <jefersonfranco@pm.me>
2023-03-01 09:50:15 -0300

    chore: delete main4.js

--- javascript/codesignal/isLucky/main4.js
+++
@@ -1,6 +0,0 @@
-function solution(n) {
-    let s = `${n}`;
-    return [...s.substring(0, s.length / 2)].reduce((p, v) => p + ~~v, 0) === [...s.substring(s.length / 2)].reduce((p, v) => p + ~~v, 0)
-}
-
-module.exports = solution;
