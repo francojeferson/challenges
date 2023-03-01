4d6bf21768cc5ea22456decb51a393806de373c8
jeferson-franco <jefersonfranco@pm.me>
2023-03-01 09:50:23 -0300

    chore: delete main3.js

--- javascript/codesignal/isLucky/main3.js
+++
@@ -1,8 +0,0 @@
-function solution(n) {
-    let a = (n + "").split(""), half = a.length / 2, l = 0, r = 0
-    while (a.length > half) r += +a.pop()
-    while (a.length) l += +a.pop()
-    return l === r
-}
-
-module.exports = solution;
