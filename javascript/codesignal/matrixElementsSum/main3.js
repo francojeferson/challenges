2882f470a371036be7544dcc66e60a8382e76c11
jeferson-franco <jefersonfranco@pm.me>
2023-03-02 10:15:03 -0300

    chore: delete main3.js

--- javascript/codesignal/matrixElementsSum/main3.js
+++
@@ -1,5 +0,0 @@
-function solution(m) {
-    return m.map((i, ind) => i.map((j, indJ) => [...new Array(ind).keys()].every(k => m[k][indJ] > 0) ? j : 0).reduce((x, y) => x + y)).reduce((x, y) => x + y)
-}
-
-module.exports = solution;
