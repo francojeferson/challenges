5b68d7dc008af42ed2f6af9950f0e36304fb4640
jeferson-franco <jefersonfranco@pm.me>
2023-03-02 20:09:11 -0300

    update main.js

--- javascript/codesignal/checkPalindrome/main.js
+++ javascript/codesignal/checkPalindrome/main.js
@@ -1,23 +1,12 @@
-// This function checks if a string is a palindrome. First, it removes all special characters from the string using a regular expression. Then it converts the string to lowercase. Then it compares each character in the string with the opposite character at the other end of the string. If all characters are equal, the function returns "true", otherwise it returns "false".
-// Essa função verifica se uma string é um palíndromo. Primeiro, ela remove todos os caracteres especiais da string usando uma expressão regular. Em seguida, ela converte a string para letras minúsculas. Depois, ela compara cada caractere da string com o caractere oposto na outra extremidade da string. Se todos os caracteres forem iguais, a função retornará "true", caso contrário, retornará "false".
+// This code is a function that takes in a string as an input and returns a boolean value. The function first converts the input string to lowercase and removes any non-alphanumeric characters using a regular expression. It then splits the string into individual characters, reverses the order of the characters, and rejoins them into a new string. Finally, it compares the original processed string to the reversed one and returns true if they are equal or false if they are not.
 
 function solution(inputString) {
     const regex = /[^a-zA-Z0-9]/g;
     procString = inputString.toLowerCase().replace(regex, '');
-
-    let integCheck = true;
-    for (let i = 0; i < procString.length / 2; i++) {
-        if (procString[i] !== procString[procString.length - 1 - i]) {
-            integCheck = false;
-            break;
-        }
-    }
-
-    return integCheck;
+    integCheck = procString.split('').reverse().join('');
+    if (procString === integCheck) return true;
+    else return false;
 }
-
-// Test 1: true Test 2: false Test 3: true Test 4: false Test 5: true Test 6: true Test 7: false Test 8: false Test 9: true Test 10: true What happened at execution was that the code was compiled and executed successfully. The tests were performed and the results were printed to the console.
-// Test 1: true Test 2: false Test 3: true Test 4: false Test 5: true Test 6: true Test 7: false Test 8: false Test 9: true Test 10: true O que aconteceu na execução foi que o código foi compilado e executado com sucesso. Os testes foram realizados e os resultados foram impressos no console.
 
 const assert = require('assert');
 function testSolution() {
