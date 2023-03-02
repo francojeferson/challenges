8c25840850154ba9873486e5bb595293a5f0c83e
jeferson-franco <jefersonfranco@pm.me>
2023-03-02 12:07:34 -0300

    chore: delete main4.js

--- javascript/codesignal/checkPalindrome/main4.js
+++
@@ -1,8 +0,0 @@
-function solution(inp) {
-    for (let i = 0, j = inp.length - 1; i <= j; ++i, --j) {
-        if (inp[i] !== inp[j]) return false;
-    }
-    return true;
-}
-
-module.exports = solution;
