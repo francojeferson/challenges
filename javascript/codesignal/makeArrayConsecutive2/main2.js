99eb1d1ea208bc5f3b82a31b8b3f7bfefe0804b1
jeferson-franco <jefersonfranco@pm.me>
2023-03-02 20:19:02 -0300

    update main2.js

--- javascript/codesignal/makeArrayConsecutive2/main2.js
+++ javascript/codesignal/makeArrayConsecutive2/main2.js
@@ -1,16 +1,10 @@
-// This function calculates how many numbers are missing to complete a sequence. It takes a sequence as parameter and uses the Math.max() function to find the largest number in the sequence and the Math.min() function to find the smallest number in the sequence. It then returns the result of the subtraction between the largest and smallest number in the sequence, added to 1, and subtracted by the number of elements in the sequence.
-// Essa função calcula a quantidade de números que faltam para completar uma sequência. Ela recebe como parâmetro uma sequência e usa a função Math.max() para encontrar o maior número da sequência e a função Math.min() para encontrar o menor número da sequência. Em seguida, ela retorna o resultado da subtração entre o maior e o menor número da sequência, somado a 1, e subtraído pela quantidade de elementos da sequência.
+// This code takes in an array of numbers (sequence) and returns the difference between the maximum and minimum values in the array, plus one, minus the length of the array. This can be used to find the number of unique elements in an array.
 
 function solution(sequence) {
-    let max = Math.max(...sequence);
-    let min = Math.min(...sequence);
-    return max - min + 1 - sequence.length;
+    return Math.max(...sequence) - Math.min(...sequence) + 1 - sequence.length;
 }
 
-// unit test
-// teste unitário
 const assert = require('assert');
-
 function testSolution() {
     let statues = [6, 2, 3, 8];
     assert.deepStrictEqual(solution(statues), 3);
