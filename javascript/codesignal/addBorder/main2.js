13b122eb51ec21434616db8ac045cfb0cc4e09ca
jeferson-franco <jefersonfranco@pm.me>
2023-03-17 12:30:46 -0300

    update main2.js

--- javascript/codesignal/addBorder/main2.js
+++ javascript/codesignal/addBorder/main2.js
@@ -1,21 +1,32 @@
-// This is a function named solution that takes a 2D array called picture as argument. The picture array is assumed to have the same length of strings in all its subarrays. The function adds a border of '*' around the picture array by inserting '*' characters at the beginning and end of each string in the picture array and then adding a row of ' *' at the top and bottom of the picture array.
-// The function begins by calculating the width of the final bordered image. It does this by taking the length of the first string in the picture array, then adding 2 (one for the '*' character at the beginning and one for the '*' character at the end.)
-// It then creates an array called result containing one string that is width characters long and consists entirely of '*'.
-// Next, the function iterates over each string in the picture array and adds a '*' at the beginning and end of the string before pushing it onto the result array.
-// Finally, the function pushes another string of '*' characters that is also width characters long to the result array, effectively creating the bottom border of the image.
-// The result array is then returned, which is the bordered image represented as an array of strings.
+// The provided function accepts a two-dimensional array of characters called picture. The function then prefixes and suffixes each line of the picture with an asterisk, and adds an asterisk to the beginning and end of the output array as well. The length of the output array is equal to the length of the input picture array plus two (+2). The resulting output array represents a "frame" created from the input picture array.
+// Here is a step-by-step breakdown of the code:
 
+// let width = picture[0].length + 2;
+
+// width is a variable that holds the length of the first line of the picture, plus two.
+// This is because the output array needs to have an asterisk at the beginning and end, which adds two additional characters to each line.
+
+// return [
+//   '*'.repeat(width), // Top border line
+//     ...picture.map(line => ${line}), // Picture + border lines
+//       '*'.repeat(width) // Bottom border line
+//       ];
+
+// The function returns an array that has three elements.
+// The first element is a string consisting of width number of asterisks (this creates the top border of the frame).
+// The second element is created by mapping all of the lines in the picture array to include an asterisk at the beginning and end (this creates the framed image).
+// The third element is another string consisting of width number of asterisks (this creates the bottom border of the frame).
+
+// Overall, the function takes in a two-dimensional array of characters and returns a new array that frames the original array with asterisks.
+
+// solution by jakzo
 function solution(picture) {
     let width = picture[0].length + 2;
-    let result = ['*'.repeat(width)];
-
-    for (let line of picture) {
-        result.push(`*${line}*`);
-    }
-
-    result.push('*'.repeat(width));
-
-    return result;
+    return [
+        '*'.repeat(width),
+        ...picture.map(line => `*${line}*`),
+        '*'.repeat(width)
+    ];
 }
 
 const assert = require('assert');
