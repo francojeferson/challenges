ae8d98930338226d103af74ecc1064cd789487e2
jeferson-franco <jefersonfranco@pm.me>
2023-03-17 09:37:06 -0300

    update main.js

--- javascript/codesignal/matrixElementsSum/main.js
+++ javascript/codesignal/matrixElementsSum/main.js
@@ -1,4 +1,12 @@
-// This code is a function that takes in a matrix (an array of arrays) as an argument. It iterates through the matrix, and if it finds a 0 in the current row, it sets the value in the same column of the next row to 0. It then adds up all of the values in the matrix and returns the sum.
+// This function receives a matrix as input parameter and returns the sum of its elements after modifying some of them. Specifically, if an element in the matrix is 0 and there is a row below it, the element in the row below is also set to 0. The function works as follows:
+
+// Initialize a variable s to 0 to keep track of the sum of the matrix elements.
+// Loop through each row of the matrix (using i) and within each row loop through each column (using j).
+// If the current element (matrix[i][j]) is 0 and there is a row below it (i + 1 < matrix.length), then set the element in the row below (matrix[i + 1][j]) to 0 as well.
+// Add the current element to the sum s.
+// After all the elements of the matrix have been visited, return the value of s.
+
+// This function is likely being used to solve a specific problem related to modifying a matrix in a particular way and calculating its final sum.
 
 function solution(matrix) {
     let s = 0;
