d7e25cba412c38ad843e15917cc29b79d8eba648
jeferson-franco <jefersonfranco@pm.me>
2023-03-17 11:10:02 -0300

    update main2.js

--- javascript/codesignal/avoidObstacles/main2.js
+++ javascript/codesignal/avoidObstacles/main2.js
@@ -1,5 +1,6 @@
-// This function checks if an element of a given array is divisible by 2. If it is, the variable 'jump' is incremented by 1 and the iteration is restarted. When no element is divisible by 'jump', the function returns the value of 'jump'.
-// Essa função verifica se um elemento de um determinado array é divisível por 2. Se for, a variável 'jump' é incrementada em 1 e a iteração é reiniciada. Quando nenhum elemento for divisível por 'jump', a função retorna o valor de 'jump'.
+// This code defines a function called solution that takes an input array as an argument. The function initializes a variable jump to 2, then loops through each element of the input array using a for loop. For each element of the array, the code checks if the element is evenly divisible by jump. If it is, the code increments jump by 1, and sets the loop iterator i back to -1 (which will be incremented to 0 on the next iteration).
+// The effect of this is that if an element of the array is divisible by jump, the code will re-check every element of the array from the beginning. This process will continue until an element is found that is not divisible by jump. Once this happens, the loop will continue onto the next element of the array, incrementing jump as necessary.
+// The function then returns the value of jump once the loop has completed. The output of the function is the minimum number that jump must be incremented to in order to ensure that no element of inputArray is divisible by jump.
 
 function solution(inputArray) {
     let jump = 2;
@@ -12,10 +13,7 @@ function solution(inputArray) {
     return jump;
 }
 
-// unit test
-// teste unitário
 const assert = require('assert');
-
 function testSolution() {
     let inputArray = [5, 3, 6, 7, 9];
     assert.deepStrictEqual(solution(inputArray), 4);
