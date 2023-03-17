c515ae7b17e22279506e9e0de548ed5e5db454ff
jeferson-franco <jefersonfranco@pm.me>
2023-03-17 13:12:50 -0300

    update main.js

--- javascript/codesignal/chessBoardCellColor/main.js
+++ javascript/codesignal/chessBoardCellColor/main.js
@@ -1,16 +1,14 @@
-// This function compares whether the cells of two chessboards are the same color. It does this by checking the ASCII code of the first character of the string given for each cell and checking to see if it is even or odd. If the two results are equal, this means that both cells are the same color and the function will return true; otherwise it will return false.
-// Essa função compara se as células de dois tabuleiros de xadrez são da mesma cor. Ela faz isso verificando o código ASCII do primeiro caractere da string dada para cada célula e verificando se é par ou ímpar. Se os dois resultados forem iguais, isso significa que ambas as células são da mesma cor e a função retornará verdadeiro; caso contrário, ela retornará falso.
+// The above code defines a function called solution that takes in two parameters: cell1 and cell2. These parameters are assumed to be strings representing the names of chessboard cells. The function returns a boolean value indicating whether or not the two cells have the same color on the chessboard.
+// The function calculates whether the cells are the same color by checking if their row and column positions are both either odd or even. It does this by comparing the ASCII values of the first character in each parameter (which represents the column) to the numeric value of the second character (which represents the row) for both cells, and performs a modulus operation to determine if they are both odd or even.
+// If the cells are the same color, the function returns true. Otherwise, it returns false.
 
 function solution(cell1, cell2) {
-    const cell1Color = cell1[0].charCodeAt(0) % 2 === (cell1[1] & 1);
-    const cell2Color = cell2[0].charCodeAt(0) % 2 === (cell2[1] & 1);
+    const cell1Color = cell1[0].charCodeAt(0) % 2 === cell1[1] % 2;
+    const cell2Color = cell2[0].charCodeAt(0) % 2 === cell2[1] % 2;
     return cell1Color === cell2Color;
 }
 
-// unit test
-// teste unitário
 const assert = require('assert');
-
 function testSolution() {
     let cell1 = "A1";
     let cell2 = "C3";
