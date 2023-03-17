0478ec184517676721ef118fa5c327f2cc468c28
jeferson-franco <jefersonfranco@pm.me>
2023-03-17 13:13:13 -0300

    update main2.js

--- javascript/codesignal/evenDigitsOnly/main2.js
+++ javascript/codesignal/evenDigitsOnly/main2.js
@@ -1,35 +1,13 @@
-// This code checks if a given number is divisible by 10 and contains only even digits. If the number is 0, it returns true. Otherwise, the function divides the number by 10 and checks if the remainder of the division by 2 is 0. If it is not, it returns false. If it is, it divides the number by 10 and rounds down to continue the check. If all operations are completed successfully, the function returns true.
-// Este código verifica se um determinado número é divisível por 10 e contém apenas dígitos pares. Se o número for 0, retornará true. Caso contrário, a função divide o número por 10 e verifica se o restante da divisão por 2 é 0. Se não for, retorna false. Se for, divide o número por 10 e arredonda para baixo para continuar a verificação. Se todas as operações forem concluídas com sucesso, a função retornará true.
+// This code defines a function called solution that takes in a parameter n. It then converts n into a string by concatenating it with an empty string ('') using the + operator. This way, the method .match() can be used to check for a pattern.
+// The regular expression /[13579]/ checks whether the string has any odd digit between 1 to 9. The ! operator negates the output, so if the string has an odd digit between 1 to 9, the function will return false. Otherwise, it will return true.
+// Overall, this code determines whether the input number n has only even digits.
 
+// solution by toby_t1
 function solution(n) {
-    // Returns true if number is 0
-    // Retorna verdadeiro se o número for 0
-    if (n === 0) return true;
-
-    // Check if the number is divisible by 10
-    // Verifica se o número é divisível por 10
-    while (n % 10 === 0) {
-        n /= 10;
-    }
-
-    while (n > 0) {
-        // If the remainder of the division by 2 is not 0, returns false
-        // Se o resto da divisão por 2 não for 0, retorna falso
-        if (n % 2 !== 0) return false;
-        // Divides the number by 10 and rounds down
-        // Divide o número por 10 e arredonda para baixo
-        n = Math.floor(n / 10);
-    }
-
-    // Returns true if all operations are checked successfully
-    // Retorna verdadeiro se todas as operações forem verificadas com sucesso
-    return true;
+    return !(n + '').match(/[13579]/);
 }
 
-// unit test
-// teste unitário
 const assert = require('assert');
-
 function testSolution() {
     let n = 248622;
     assert.deepStrictEqual(solution(n), true);
