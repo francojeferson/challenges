cd87a07910666adfa09b9c3880730c0a5e287b87
jeferson-franco <jefersonfranco@pm.me>
2023-03-19 09:37:02 -0300

    update main2.js

--- javascript/codesignal/depositProfit/main2.js
+++ javascript/codesignal/depositProfit/main2.js
@@ -1,14 +1,14 @@
-// This function calculates how many years it will take for a given deposit to reach a limit value. The function takes as arguments the initial deposit amount, the interest rate, and the threshold amount. It uses the formula ln (threshold / deposit) / ln (1 + rate / 100), where ln is a natural logarithmic function, to calculate how many years it will take to reach the threshold value. The function Math.ceil rounds up the result and returns the integer number of years needed.
-// Esta função calcula quantos anos levará para que um determinado depósito alcance um valor limite. A função recebe como argumentos o valor inicial do depósito, a taxa de juros e o valor limite. Ela usa a fórmula ln (threshold / deposit) / ln (1 + rate / 100), onde ln é uma função natural logarítmica, para calcular quantos anos levará para alcançar o valor limite. A função Math.ceil arredonda o resultado para cima e retorna o número inteiro de anos necessários.
+// This code defines a function solution which takes in three parameters: deposit, rate, and threshold. The function computes the number of years, rounded up to the nearest integer, that it takes for an initial deposit (deposit) with a certain interest rate (rate) to reach a certain threshold balance (threshold), assuming that the interest compounds continuously.
+// The first part of the return statement Math.log(threshold / deposit) computes the natural logarithm of the ratio of the target threshold balance to the initial deposit.
+// The second part of the return statement Math.log(1 + rate / 100) computes the natural logarithm of the interest rate plus one (to account for compounding).
+// The two logarithms are divided to obtain the number of compounding periods required to reach the threshold balance, and Math.ceil is used to round up to the nearest integer, representing the number of years it takes to reach the threshold balance.
 
+// solution by ivan_d
 function solution(deposit, rate, threshold) {
     return Math.ceil(Math.log(threshold / deposit) / Math.log(1 + rate / 100));
 }
 
-// unit test
-// teste unitário
 const assert = require('assert');
-
 function testSolution() {
     let deposit = 100;
     let rate = 20;
