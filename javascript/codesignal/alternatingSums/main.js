ef800499c962db94a5c05fafea78a6bed6c09034
jeferson-franco <jefersonfranco@pm.me>
2023-03-31 18:05:49 -0300

    update main.js

--- javascript/codesignal/alternatingSums/main.js
+++ javascript/codesignal/alternatingSums/main.js
@@ -15,25 +15,29 @@ function solution(a) {
 
 const assert = require('assert');
 function testSolution() {
-    let a = [50, 60, 60, 45, 70];
-    assert.deepStrictEqual(solution(a), [180, 105]);
-    console.log('Test 1: ', solution(a));
+    try {
+        let a = [50, 60, 60, 45, 70];
+        assert.deepStrictEqual(solution(a), [180, 105]);
+        console.log('Test 1: ', solution(a));
 
-    a = [100, 50];
-    assert.deepStrictEqual(solution(a), [100, 50]);
-    console.log('Test 2: ', solution(a));
+        a = [100, 50];
+        assert.deepStrictEqual(solution(a), [100, 50]);
+        console.log('Test 2: ', solution(a));
 
-    a = [80];
-    assert.deepStrictEqual(solution(a), [80, 0]);
-    console.log('Test 3: ', solution(a));
+        a = [80];
+        assert.deepStrictEqual(solution(a), [80, 0]);
+        console.log('Test 3: ', solution(a));
 
-    a = [100, 50, 50, 100];
-    assert.deepStrictEqual(solution(a), [150, 150]);
-    console.log('Test 4: ', solution(a));
+        a = [100, 50, 50, 100];
+        assert.deepStrictEqual(solution(a), [150, 150]);
+        console.log('Test 4: ', solution(a));
 
-    a = [100, 51, 50, 100];
-    assert.deepStrictEqual(solution(a), [150, 151]);
-    console.log('Test 5: ', solution(a));
+        a = [100, 51, 50, 100];
+        assert.deepStrictEqual(solution(a), [150, 151]);
+        console.log('Test 5: ', solution(a));
+    } catch (e) {
+        console.log(e.name, e.message);
+    }
 }
 
 testSolution();
