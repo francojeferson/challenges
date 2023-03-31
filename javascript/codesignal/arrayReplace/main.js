b941749b24eb2c4eb189a1ae75b89082ef71987e
jeferson-franco <jefersonfranco@pm.me>
2023-03-31 18:07:29 -0300

    update main.js

--- javascript/codesignal/arrayReplace/main.js
+++ javascript/codesignal/arrayReplace/main.js
@@ -22,53 +22,57 @@ function solution(inputArray, elemToReplace, substitutionElem) {
 
 const assert = require('assert');
 function testSolution() {
-    let inputArray = [1, 2, 1];
-    let elemToReplace = 1;
-    let substitutionElem = 3;
-    assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [3, 2, 3]);
-    console.log('Test 1: ', solution(inputArray, elemToReplace, substitutionElem));
+    try {
+        let inputArray = [1, 2, 1];
+        let elemToReplace = 1;
+        let substitutionElem = 3;
+        assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [3, 2, 3]);
+        console.log('Test 1: ', solution(inputArray, elemToReplace, substitutionElem));
 
-    inputArray = [1, 2, 3, 4, 5];
-    elemToReplace = 3;
-    substitutionElem = 0;
-    assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [1, 2, 0, 4, 5]);
-    console.log('Test 2: ', solution(inputArray, elemToReplace, substitutionElem));
+        inputArray = [1, 2, 3, 4, 5];
+        elemToReplace = 3;
+        substitutionElem = 0;
+        assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [1, 2, 0, 4, 5]);
+        console.log('Test 2: ', solution(inputArray, elemToReplace, substitutionElem));
 
-    inputArray = [1, 1, 1];
-    elemToReplace = 1;
-    substitutionElem = 10;
-    assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [10, 10, 10]);
-    console.log('Test 3: ', solution(inputArray, elemToReplace, substitutionElem));
+        inputArray = [1, 1, 1];
+        elemToReplace = 1;
+        substitutionElem = 10;
+        assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [10, 10, 10]);
+        console.log('Test 3: ', solution(inputArray, elemToReplace, substitutionElem));
 
-    inputArray = [1, 2, 1, 2, 1];
-    elemToReplace = 2;
-    substitutionElem = 1;
-    assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [1, 1, 1, 1, 1]);
-    console.log('Test 4: ', solution(inputArray, elemToReplace, substitutionElem));
+        inputArray = [1, 2, 1, 2, 1];
+        elemToReplace = 2;
+        substitutionElem = 1;
+        assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [1, 1, 1, 1, 1]);
+        console.log('Test 4: ', solution(inputArray, elemToReplace, substitutionElem));
 
-    inputArray = [1, 2, 1, 2, 1];
-    elemToReplace = 2;
-    substitutionElem = 2;
-    assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [1, 2, 1, 2, 1]);
-    console.log('Test 5: ', solution(inputArray, elemToReplace, substitutionElem));
+        inputArray = [1, 2, 1, 2, 1];
+        elemToReplace = 2;
+        substitutionElem = 2;
+        assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [1, 2, 1, 2, 1]);
+        console.log('Test 5: ', solution(inputArray, elemToReplace, substitutionElem));
 
-    inputArray = [3, 1];
-    elemToReplace = 3;
-    substitutionElem = 9;
-    assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [9, 1]);
-    console.log('Test 6: ', solution(inputArray, elemToReplace, substitutionElem));
+        inputArray = [3, 1];
+        elemToReplace = 3;
+        substitutionElem = 9;
+        assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [9, 1]);
+        console.log('Test 6: ', solution(inputArray, elemToReplace, substitutionElem));
 
-    inputArray = [10, 10];
-    elemToReplace = 0;
-    substitutionElem = 9;
-    assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [10, 10]);
-    console.log('Test 7: ', solution(inputArray, elemToReplace, substitutionElem));
+        inputArray = [10, 10];
+        elemToReplace = 0;
+        substitutionElem = 9;
+        assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [10, 10]);
+        console.log('Test 7: ', solution(inputArray, elemToReplace, substitutionElem));
 
-    inputArray = [2, 1];
-    elemToReplace = 3;
-    substitutionElem = 9;
-    assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [2, 1]);
-    console.log('Test 8: ', solution(inputArray, elemToReplace, substitutionElem));
+        inputArray = [2, 1];
+        elemToReplace = 3;
+        substitutionElem = 9;
+        assert.deepStrictEqual(solution(inputArray, elemToReplace, substitutionElem), [2, 1]);
+        console.log('Test 8: ', solution(inputArray, elemToReplace, substitutionElem));
+    } catch (e) {
+        console.log(e.name, e.message);
+    }
 }
 
 testSolution();
