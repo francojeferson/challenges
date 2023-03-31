5fd768d0103c0cf9ae5c471070eb2605a116e356
jeferson-franco <jefersonfranco@pm.me>
2023-03-31 17:59:52 -0300

    update main.js

--- javascript/codesignal/add/main.js
+++ javascript/codesignal/add/main.js
@@ -7,35 +7,39 @@ function solution(param1, param2) {
 
 const assert = require('assert');
 function testSolution() {
-    let param1 = 1;
-    let param2 = 2;
-    assert.deepStrictEqual(solution(param1, param2), 3);
-    console.log('Test 1: ', solution(param1, param2));
+    try {
+        let param1 = 1;
+        let param2 = 2;
+        assert.deepStrictEqual(solution(param1, param2), 3);
+        console.log('Test 1: ', solution(param1, param2));
 
-    param1 = 0;
-    param2 = 1000;
-    assert.deepStrictEqual(solution(param1, param2), 1000);
-    console.log('Test 2: ', solution(param1, param2));
+        param1 = 0;
+        param2 = 1000;
+        assert.deepStrictEqual(solution(param1, param2), 1000);
+        console.log('Test 2: ', solution(param1, param2));
 
-    param1 = 2;
-    param2 = -39;
-    assert.deepStrictEqual(solution(param1, param2), -37);
-    console.log('Test 3: ', solution(param1, param2));
+        param1 = 2;
+        param2 = -39;
+        assert.deepStrictEqual(solution(param1, param2), -37);
+        console.log('Test 3: ', solution(param1, param2));
 
-    param1 = 99;
-    param2 = 100;
-    assert.deepStrictEqual(solution(param1, param2), 199);
-    console.log('Test 4: ', solution(param1, param2));
+        param1 = 99;
+        param2 = 100;
+        assert.deepStrictEqual(solution(param1, param2), 199);
+        console.log('Test 4: ', solution(param1, param2));
 
-    param1 = -100;
-    param2 = 100;
-    assert.deepStrictEqual(solution(param1, param2), 0);
-    console.log('Test 5: ', solution(param1, param2));
+        param1 = -100;
+        param2 = 100;
+        assert.deepStrictEqual(solution(param1, param2), 0);
+        console.log('Test 5: ', solution(param1, param2));
 
-    param1 = -1000;
-    param2 = -1000;
-    assert.deepStrictEqual(solution(param1, param2), -2000);
-    console.log('Test 6: ', solution(param1, param2));
+        param1 = -1000;
+        param2 = -1000;
+        assert.deepStrictEqual(solution(param1, param2), -2000);
+        console.log('Test 6: ', solution(param1, param2));
+    } catch (e) {
+        console.log(e.name, e.message);
+    }
 }
 
 testSolution();
