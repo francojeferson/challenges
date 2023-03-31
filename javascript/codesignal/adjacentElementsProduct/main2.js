fa355577f28a7ca3b84a471efce30c93dce11c1e
jeferson-franco <jefersonfranco@pm.me>
2023-03-31 18:02:06 -0300

    update main2.js

--- javascript/codesignal/adjacentElementsProduct/main2.js
+++ javascript/codesignal/adjacentElementsProduct/main2.js
@@ -15,41 +15,45 @@ function solution(inputArray) {
 
 const assert = require('assert');
 function testSolution() {
-    let inputArray = [3, 6, -2, -5, 7, 3];
-    assert.deepStrictEqual(solution(inputArray), 21);
-    console.log('Test 1: ', solution(inputArray));
+    try {
+        let inputArray = [3, 6, -2, -5, 7, 3];
+        assert.deepStrictEqual(solution(inputArray), 21);
+        console.log('Test 1: ', solution(inputArray));
 
-    inputArray = [-1, -2];
-    assert.deepStrictEqual(solution(inputArray), 2);
-    console.log('Test 2: ', solution(inputArray));
+        inputArray = [-1, -2];
+        assert.deepStrictEqual(solution(inputArray), 2);
+        console.log('Test 2: ', solution(inputArray));
 
-    inputArray = [5, 1, 2, 3, 1, 4];
-    assert.deepStrictEqual(solution(inputArray), 6);
-    console.log('Test 3: ', solution(inputArray));
+        inputArray = [5, 1, 2, 3, 1, 4];
+        assert.deepStrictEqual(solution(inputArray), 6);
+        console.log('Test 3: ', solution(inputArray));
 
-    inputArray = [1, 2, 3, 0];
-    assert.deepStrictEqual(solution(inputArray), 6);
-    console.log('Test 4: ', solution(inputArray));
+        inputArray = [1, 2, 3, 0];
+        assert.deepStrictEqual(solution(inputArray), 6);
+        console.log('Test 4: ', solution(inputArray));
 
-    inputArray = [9, 5, 10, 2, 24, -1, -48];
-    assert.deepStrictEqual(solution(inputArray), 50);
-    console.log('Test 5: ', solution(inputArray));
+        inputArray = [9, 5, 10, 2, 24, -1, -48];
+        assert.deepStrictEqual(solution(inputArray), 50);
+        console.log('Test 5: ', solution(inputArray));
 
-    inputArray = [5, 6, -4, 2, 3, 2, -23];
-    assert.deepStrictEqual(solution(inputArray), 30);
-    console.log('Test 6: ', solution(inputArray));
+        inputArray = [5, 6, -4, 2, 3, 2, -23];
+        assert.deepStrictEqual(solution(inputArray), 30);
+        console.log('Test 6: ', solution(inputArray));
 
-    inputArray = [4, 1, 2, 3, 1, 5];
-    assert.deepStrictEqual(solution(inputArray), 6);
-    console.log('Test 7: ', solution(inputArray));
+        inputArray = [4, 1, 2, 3, 1, 5];
+        assert.deepStrictEqual(solution(inputArray), 6);
+        console.log('Test 7: ', solution(inputArray));
 
-    inputArray = [-23, 4, -3, 8, -12];
-    assert.deepStrictEqual(solution(inputArray), -12);
-    console.log('Test 8: ', solution(inputArray));
+        inputArray = [-23, 4, -3, 8, -12];
+        assert.deepStrictEqual(solution(inputArray), -12);
+        console.log('Test 8: ', solution(inputArray));
 
-    inputArray = [1, 0, 1, 0, 1000];
-    assert.deepStrictEqual(solution(inputArray), 0);
-    console.log('Test 9: ', solution(inputArray));
+        inputArray = [1, 0, 1, 0, 1000];
+        assert.deepStrictEqual(solution(inputArray), 0);
+        console.log('Test 9: ', solution(inputArray));
+    } catch (e) {
+        console.log(e.name, e.message);
+    }
 }
 
 testSolution();
