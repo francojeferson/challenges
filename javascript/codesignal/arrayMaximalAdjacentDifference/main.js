f0e7cbae9ad2f3ec0a32625f8edbecc87a515e57
jeferson-franco <jefersonfranco@pm.me>
2023-03-31 18:07:13 -0300

    update main.js

--- javascript/codesignal/arrayMaximalAdjacentDifference/main.js
+++ javascript/codesignal/arrayMaximalAdjacentDifference/main.js
@@ -13,33 +13,37 @@ function solution(inputArray) {
 
 const assert = require('assert');
 function testSolution() {
-    let inputArray = [2, 4, 1, 0];
-    assert.deepStrictEqual(solution(inputArray), 3);
-    console.log('Test 1: ', solution(inputArray));
+    try {
+        let inputArray = [2, 4, 1, 0];
+        assert.deepStrictEqual(solution(inputArray), 3);
+        console.log('Test 1: ', solution(inputArray));
 
-    inputArray = [1, 1, 1, 1];
-    assert.deepStrictEqual(solution(inputArray), 0);
-    console.log('Test 2: ', solution(inputArray));
+        inputArray = [1, 1, 1, 1];
+        assert.deepStrictEqual(solution(inputArray), 0);
+        console.log('Test 2: ', solution(inputArray));
 
-    inputArray = [-1, 4, 10, 3, -2];
-    assert.deepStrictEqual(solution(inputArray), 7);
-    console.log('Test 3: ', solution(inputArray));
+        inputArray = [-1, 4, 10, 3, -2];
+        assert.deepStrictEqual(solution(inputArray), 7);
+        console.log('Test 3: ', solution(inputArray));
 
-    inputArray = [10, 11, 13];
-    assert.deepStrictEqual(solution(inputArray), 2);
-    console.log('Test 4: ', solution(inputArray));
+        inputArray = [10, 11, 13];
+        assert.deepStrictEqual(solution(inputArray), 2);
+        console.log('Test 4: ', solution(inputArray));
 
-    inputArray = [-2, -2, -2, -2, -2];
-    assert.deepStrictEqual(solution(inputArray), 0);
-    console.log('Test 5: ', solution(inputArray));
+        inputArray = [-2, -2, -2, -2, -2];
+        assert.deepStrictEqual(solution(inputArray), 0);
+        console.log('Test 5: ', solution(inputArray));
 
-    inputArray = [-1, 1, -3, -4];
-    assert.deepStrictEqual(solution(inputArray), 4);
-    console.log('Test 6: ', solution(inputArray));
+        inputArray = [-1, 1, -3, -4];
+        assert.deepStrictEqual(solution(inputArray), 4);
+        console.log('Test 6: ', solution(inputArray));
 
-    inputArray = [-14, 15, -15];
-    assert.deepStrictEqual(solution(inputArray), 30);
-    console.log('Test 7: ', solution(inputArray));
+        inputArray = [-14, 15, -15];
+        assert.deepStrictEqual(solution(inputArray), 30);
+        console.log('Test 7: ', solution(inputArray));
+    } catch (e) {
+        console.log(e.name, e.message);
+    }
 }
 
 testSolution();
