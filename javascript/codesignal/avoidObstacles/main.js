42ec1fb11786fc9c8eb3c8872b15ef8c9ef631ee
jeferson-franco <jefersonfranco@pm.me>
2023-03-31 18:07:44 -0300

    update main.js

--- javascript/codesignal/avoidObstacles/main.js
+++ javascript/codesignal/avoidObstacles/main.js
@@ -15,29 +15,33 @@ function solution(inputArray) {
 
 const assert = require('assert');
 function testSolution() {
-    let inputArray = [5, 3, 6, 7, 9];
-    assert.deepStrictEqual(solution(inputArray), 4);
-    console.log('Test 1: ', solution(inputArray));
+    try {
+        let inputArray = [5, 3, 6, 7, 9];
+        assert.deepStrictEqual(solution(inputArray), 4);
+        console.log('Test 1: ', solution(inputArray));
 
-    inputArray = [2, 3];
-    assert.deepStrictEqual(solution(inputArray), 4);
-    console.log('Test 2: ', solution(inputArray));
+        inputArray = [2, 3];
+        assert.deepStrictEqual(solution(inputArray), 4);
+        console.log('Test 2: ', solution(inputArray));
 
-    inputArray = [1, 4, 10, 6, 2];
-    assert.deepStrictEqual(solution(inputArray), 7);
-    console.log('Test 3: ', solution(inputArray));
+        inputArray = [1, 4, 10, 6, 2];
+        assert.deepStrictEqual(solution(inputArray), 7);
+        console.log('Test 3: ', solution(inputArray));
 
-    inputArray = [1000, 999];
-    assert.deepStrictEqual(solution(inputArray), 6);
-    console.log('Test 4: ', solution(inputArray));
+        inputArray = [1000, 999];
+        assert.deepStrictEqual(solution(inputArray), 6);
+        console.log('Test 4: ', solution(inputArray));
 
-    inputArray = [19, 32, 11, 23];
-    assert.deepStrictEqual(solution(inputArray), 3);
-    console.log('Test 5: ', solution(inputArray));
+        inputArray = [19, 32, 11, 23];
+        assert.deepStrictEqual(solution(inputArray), 3);
+        console.log('Test 5: ', solution(inputArray));
 
-    inputArray = [5, 8, 9, 13, 14];
-    assert.deepStrictEqual(solution(inputArray), 6);
-    console.log('Test 6: ', solution(inputArray));
+        inputArray = [5, 8, 9, 13, 14];
+        assert.deepStrictEqual(solution(inputArray), 6);
+        console.log('Test 6: ', solution(inputArray));
+    } catch (e) {
+        console.log(e.name, e.message);
+    }
 }
 
 testSolution();
