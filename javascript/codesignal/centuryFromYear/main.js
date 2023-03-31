82f56656b3f94cda04f7896b9df6ed5c16c8b338
jeferson-franco <jefersonfranco@pm.me>
2023-03-31 18:09:01 -0300

    update main.js

--- javascript/codesignal/centuryFromYear/main.js
+++ javascript/codesignal/centuryFromYear/main.js
@@ -6,41 +6,45 @@ function solution(year) {
 
 const assert = require('assert');
 function testSolution() {
-    let year = 1905;
-    assert.deepStrictEqual(solution(year), 20);
-    console.log('Test 1: ', solution(year));
+    try {
+        let year = 1905;
+        assert.deepStrictEqual(solution(year), 20);
+        console.log('Test 1: ', solution(year));
 
-    year = 1700;
-    assert.deepStrictEqual(solution(year), 17);
-    console.log('Test 2: ', solution(year));
+        year = 1700;
+        assert.deepStrictEqual(solution(year), 17);
+        console.log('Test 2: ', solution(year));
 
-    year = 1988;
-    assert.deepStrictEqual(solution(year), 20);
-    console.log('Test 3: ', solution(year));
+        year = 1988;
+        assert.deepStrictEqual(solution(year), 20);
+        console.log('Test 3: ', solution(year));
 
-    year = 2000;
-    assert.deepStrictEqual(solution(year), 20);
-    console.log('Test 4: ', solution(year));
+        year = 2000;
+        assert.deepStrictEqual(solution(year), 20);
+        console.log('Test 4: ', solution(year));
 
-    year = 2001;
-    assert.deepStrictEqual(solution(year), 21);
-    console.log('Test 5: ', solution(year));
+        year = 2001;
+        assert.deepStrictEqual(solution(year), 21);
+        console.log('Test 5: ', solution(year));
 
-    year = 200;
-    assert.deepStrictEqual(solution(year), 2);
-    console.log('Test 6: ', solution(year));
+        year = 200;
+        assert.deepStrictEqual(solution(year), 2);
+        console.log('Test 6: ', solution(year));
 
-    year = 374;
-    assert.deepStrictEqual(solution(year), 4);
-    console.log('Test 7: ', solution(year));
+        year = 374;
+        assert.deepStrictEqual(solution(year), 4);
+        console.log('Test 7: ', solution(year));
 
-    year = 45;
-    assert.deepStrictEqual(solution(year), 1);
-    console.log('Test 8: ', solution(year));
+        year = 45;
+        assert.deepStrictEqual(solution(year), 1);
+        console.log('Test 8: ', solution(year));
 
-    year = 8;
-    assert.deepStrictEqual(solution(year), 1);
-    console.log('Test 9: ', solution(year));
+        year = 8;
+        assert.deepStrictEqual(solution(year), 1);
+        console.log('Test 9: ', solution(year));
+    } catch (e) {
+        console.log(e.name, e.message);
+    }
 }
 
 testSolution();
