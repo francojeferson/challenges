fb4dddf5e29f1de8d1371b40792e898c9c079403
jeferson-franco <jefersonfranco@pm.me>
2023-03-31 18:06:34 -0300

    update main2.js

--- javascript/codesignal/areSimilar/main2.js
+++ javascript/codesignal/areSimilar/main2.js
@@ -9,55 +9,59 @@ function solution(a, b) {
 
 const assert = require('assert');
 function testSolution() {
-    let a = [1, 2, 3];
-    let b = [1, 2, 3];
-    assert.deepStrictEqual(solution(a, b), true);
-    console.log('Test 1: ', solution(a, b));
+    try {
+        let a = [1, 2, 3];
+        let b = [1, 2, 3];
+        assert.deepStrictEqual(solution(a, b), true);
+        console.log('Test 1: ', solution(a, b));
 
-    a = [1, 2, 3];
-    b = [2, 1, 3];
-    assert.deepStrictEqual(solution(a, b), true);
-    console.log('Test 2: ', solution(a, b));
+        a = [1, 2, 3];
+        b = [2, 1, 3];
+        assert.deepStrictEqual(solution(a, b), true);
+        console.log('Test 2: ', solution(a, b));
 
-    a = [1, 2, 2];
-    b = [2, 1, 1];
-    assert.deepStrictEqual(solution(a, b), false);
-    console.log('Test 3: ', solution(a, b));
+        a = [1, 2, 2];
+        b = [2, 1, 1];
+        assert.deepStrictEqual(solution(a, b), false);
+        console.log('Test 3: ', solution(a, b));
 
-    a = [1, 2, 1, 2];
-    b = [2, 2, 1, 1];
-    assert.deepStrictEqual(solution(a, b), true);
-    console.log('Test 4: ', solution(a, b));
+        a = [1, 2, 1, 2];
+        b = [2, 2, 1, 1];
+        assert.deepStrictEqual(solution(a, b), true);
+        console.log('Test 4: ', solution(a, b));
 
-    a = [1, 2, 1, 2, 2, 1];
-    b = [2, 2, 1, 1, 2, 1];
-    assert.deepStrictEqual(solution(a, b), true);
-    console.log('Test 5: ', solution(a, b));
+        a = [1, 2, 1, 2, 2, 1];
+        b = [2, 2, 1, 1, 2, 1];
+        assert.deepStrictEqual(solution(a, b), true);
+        console.log('Test 5: ', solution(a, b));
 
-    a = [1, 1, 4];
-    b = [1, 2, 3];
-    assert.deepStrictEqual(solution(a, b), false);
-    console.log('Test 6: ', solution(a, b));
+        a = [1, 1, 4];
+        b = [1, 2, 3];
+        assert.deepStrictEqual(solution(a, b), false);
+        console.log('Test 6: ', solution(a, b));
 
-    a = [1, 2, 3];
-    b = [1, 10, 2];
-    assert.deepStrictEqual(solution(a, b), false);
-    console.log('Test 7: ', solution(a, b));
+        a = [1, 2, 3];
+        b = [1, 10, 2];
+        assert.deepStrictEqual(solution(a, b), false);
+        console.log('Test 7: ', solution(a, b));
 
-    a = [2, 3, 1];
-    b = [1, 3, 2];
-    assert.deepStrictEqual(solution(a, b), true);
-    console.log('Test 8: ', solution(a, b));
+        a = [2, 3, 1];
+        b = [1, 3, 2];
+        assert.deepStrictEqual(solution(a, b), true);
+        console.log('Test 8: ', solution(a, b));
 
-    a = [2, 3, 9];
-    b = [10, 3, 2];
-    assert.deepStrictEqual(solution(a, b), false);
-    console.log('Test 9: ', solution(a, b));
+        a = [2, 3, 9];
+        b = [10, 3, 2];
+        assert.deepStrictEqual(solution(a, b), false);
+        console.log('Test 9: ', solution(a, b));
 
-    a = [832, 998, 148, 570, 533, 561, 894, 147, 455, 279];
-    b = [832, 570, 148, 998, 533, 561, 455, 147, 894, 279];
-    assert.deepStrictEqual(solution(a, b), false);
-    console.log('Test 10: ', solution(a, b));
+        a = [832, 998, 148, 570, 533, 561, 894, 147, 455, 279];
+        b = [832, 570, 148, 998, 533, 561, 455, 147, 894, 279];
+        assert.deepStrictEqual(solution(a, b), false);
+        console.log('Test 10: ', solution(a, b));
+    } catch (e) {
+        console.log(e.name, e.message);
+    }
 }
 
 testSolution();
