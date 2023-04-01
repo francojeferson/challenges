d0fa5683254dcf482a960da51afc7921980965e5
jeferson-franco <jefersonfranco@pm.me>
2023-03-31 22:46:22 -0300

    update main2.js

--- javascript/codesignal/validTime/main2.js
+++ javascript/codesignal/validTime/main2.js
@@ -7,49 +7,53 @@ function solution(time) {
 
 const assert = require('assert');
 function testSolution() {
-    let time = "13:58";
-    assert.deepStrictEqual(solution(time), true);
-    console.log('Test 1: ', solution(time));
+    try {
+        let time = "13:58";
+        assert.deepStrictEqual(solution(time), true);
+        console.log('Test 1: ', solution(time));
 
-    time = "25:51";
-    assert.deepStrictEqual(solution(time), false);
-    console.log('Test 2: ', solution(time));
+        time = "25:51";
+        assert.deepStrictEqual(solution(time), false);
+        console.log('Test 2: ', solution(time));
 
-    time = "02:76";
-    assert.deepStrictEqual(solution(time), false);
-    console.log('Test 3: ', solution(time));
+        time = "02:76";
+        assert.deepStrictEqual(solution(time), false);
+        console.log('Test 3: ', solution(time));
 
-    time = "24:00";
-    assert.deepStrictEqual(solution(time), false);
-    console.log('Test 4: ', solution(time));
+        time = "24:00";
+        assert.deepStrictEqual(solution(time), false);
+        console.log('Test 4: ', solution(time));
 
-    time = "02:61";
-    assert.deepStrictEqual(solution(time), false);
-    console.log('Test 5: ', solution(time));
+        time = "02:61";
+        assert.deepStrictEqual(solution(time), false);
+        console.log('Test 5: ', solution(time));
 
-    time = "27:00";
-    assert.deepStrictEqual(solution(time), false);
-    console.log('Test 6: ', solution(time));
+        time = "27:00";
+        assert.deepStrictEqual(solution(time), false);
+        console.log('Test 6: ', solution(time));
 
-    time = "19:66";
-    assert.deepStrictEqual(solution(time), false);
-    console.log('Test 7: ', solution(time));
+        time = "19:66";
+        assert.deepStrictEqual(solution(time), false);
+        console.log('Test 7: ', solution(time));
 
-    time = "12:31";
-    assert.deepStrictEqual(solution(time), true);
-    console.log('Test 8: ', solution(time));
+        time = "12:31";
+        assert.deepStrictEqual(solution(time), true);
+        console.log('Test 8: ', solution(time));
 
-    time = "25:73";
-    assert.deepStrictEqual(solution(time), false);
-    console.log('Test 9: ', solution(time));
+        time = "25:73";
+        assert.deepStrictEqual(solution(time), false);
+        console.log('Test 9: ', solution(time));
 
-    time = "09:56";
-    assert.deepStrictEqual(solution(time), true);
-    console.log('Test 10: ', solution(time));
+        time = "09:56";
+        assert.deepStrictEqual(solution(time), true);
+        console.log('Test 10: ', solution(time));
 
-    time = "03:29";
-    assert.deepStrictEqual(solution(time), true);
-    console.log('Test 11: ', solution(time));
+        time = "03:29";
+        assert.deepStrictEqual(solution(time), true);
+        console.log('Test 11: ', solution(time));
+    } catch (e) {
+        console.log(e.name, e.message);
+    }
 }
 
 testSolution();
