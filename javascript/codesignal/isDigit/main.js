d5f743838eb8940336a92e5c65e22cf489cade85
jeferson-franco <jefersonfranco@pm.me>
2023-03-31 22:40:23 -0300

    update main.js

--- javascript/codesignal/isDigit/main.js
+++ javascript/codesignal/isDigit/main.js
@@ -6,49 +6,53 @@ function solution(symbol) {
 
 const assert = require('assert');
 function testSolution() {
-    let symbol = "0";
-    assert.deepStrictEqual(solution(symbol), true);
-    console.log('Test 1: ', solution(symbol));
+    try {
+        let symbol = "0";
+        assert.deepStrictEqual(solution(symbol), true);
+        console.log('Test 1: ', solution(symbol));
 
-    symbol = "-";
-    assert.deepStrictEqual(solution(symbol), false);
-    console.log('Test 2: ', solution(symbol));
+        symbol = "-";
+        assert.deepStrictEqual(solution(symbol), false);
+        console.log('Test 2: ', solution(symbol));
 
-    symbol = "O";
-    assert.deepStrictEqual(solution(symbol), false);
-    console.log('Test 3: ', solution(symbol));
+        symbol = "O";
+        assert.deepStrictEqual(solution(symbol), false);
+        console.log('Test 3: ', solution(symbol));
 
-    symbol = "1";
-    assert.deepStrictEqual(solution(symbol), true);
-    console.log('Test 4: ', solution(symbol));
+        symbol = "1";
+        assert.deepStrictEqual(solution(symbol), true);
+        console.log('Test 4: ', solution(symbol));
 
-    symbol = "2";
-    assert.deepStrictEqual(solution(symbol), true);
-    console.log('Test 5: ', solution(symbol));
+        symbol = "2";
+        assert.deepStrictEqual(solution(symbol), true);
+        console.log('Test 5: ', solution(symbol));
 
-    symbol = "!";
-    assert.deepStrictEqual(solution(symbol), false);
-    console.log('Test 6: ', solution(symbol));
+        symbol = "!";
+        assert.deepStrictEqual(solution(symbol), false);
+        console.log('Test 6: ', solution(symbol));
 
-    symbol = "@";
-    assert.deepStrictEqual(solution(symbol), false);
-    console.log('Test 7: ', solution(symbol));
+        symbol = "@";
+        assert.deepStrictEqual(solution(symbol), false);
+        console.log('Test 7: ', solution(symbol));
 
-    symbol = "+";
-    assert.deepStrictEqual(solution(symbol), false);
-    console.log('Test 8: ', solution(symbol));
+        symbol = "+";
+        assert.deepStrictEqual(solution(symbol), false);
+        console.log('Test 8: ', solution(symbol));
 
-    symbol = "6";
-    assert.deepStrictEqual(solution(symbol), true);
-    console.log('Test 9: ', solution(symbol));
+        symbol = "6";
+        assert.deepStrictEqual(solution(symbol), true);
+        console.log('Test 9: ', solution(symbol));
 
-    symbol = "(";
-    assert.deepStrictEqual(solution(symbol), false);
-    console.log('Test 10: ', solution(symbol));
+        symbol = "(";
+        assert.deepStrictEqual(solution(symbol), false);
+        console.log('Test 10: ', solution(symbol));
 
-    symbol = ")";
-    assert.deepStrictEqual(solution(symbol), false);
-    console.log('Test 11: ', solution(symbol));
+        symbol = ")";
+        assert.deepStrictEqual(solution(symbol), false);
+        console.log('Test 11: ', solution(symbol));
+    } catch (e) {
+        console.log(e.name, e.message);
+    }
 }
 
 testSolution();
