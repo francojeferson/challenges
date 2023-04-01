8e866e72ee18fa219286202d0350288a70d8cb2e
jeferson-franco <jefersonfranco@pm.me>
2023-03-31 22:44:47 -0300

    update main.js

--- javascript/codesignal/sortByHeight/main.js
+++ javascript/codesignal/sortByHeight/main.js
@@ -28,29 +28,33 @@ function solution(a) {
 
 const assert = require('assert');
 function testSolution() {
-    let a = [-1, 150, 190, 170, -1, -1, 160, 180];
-    assert.deepStrictEqual(solution(a), [-1, 150, 160, 170, -1, -1, 180, 190]);
-    console.log('Test 1: ', solution(a));
+    try {
+        let a = [-1, 150, 190, 170, -1, -1, 160, 180];
+        assert.deepStrictEqual(solution(a), [-1, 150, 160, 170, -1, -1, 180, 190]);
+        console.log('Test 1: ', solution(a));
 
-    a = [-1, -1, -1, -1, -1];
-    assert.deepStrictEqual(solution(a), [-1, -1, -1, -1, -1]);
-    console.log('Test 2: ', solution(a));
+        a = [-1, -1, -1, -1, -1];
+        assert.deepStrictEqual(solution(a), [-1, -1, -1, -1, -1]);
+        console.log('Test 2: ', solution(a));
 
-    a = [-1];
-    assert.deepStrictEqual(solution(a), [-1]);
-    console.log('Test 3: ', solution(a));
+        a = [-1];
+        assert.deepStrictEqual(solution(a), [-1]);
+        console.log('Test 3: ', solution(a));
 
-    a = [4, 2, 9, 11, 2, 16];
-    assert.deepStrictEqual(solution(a), [2, 2, 4, 9, 11, 16]);
-    console.log('Test 4: ', solution(a));
+        a = [4, 2, 9, 11, 2, 16];
+        assert.deepStrictEqual(solution(a), [2, 2, 4, 9, 11, 16]);
+        console.log('Test 4: ', solution(a));
 
-    a = [2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1];
-    assert.deepStrictEqual(solution(a), [1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2]);
-    console.log('Test 5: ', solution(a));
+        a = [2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1];
+        assert.deepStrictEqual(solution(a), [1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 2]);
+        console.log('Test 5: ', solution(a));
 
-    a = [23, 54, -1, 43, 1, -1, -1, 77, -1, -1, -1, 3];
-    assert.deepStrictEqual(solution(a), [1, 3, -1, 23, 43, -1, -1, 54, -1, -1, -1, 77]);
-    console.log('Test 6: ', solution(a));
+        a = [23, 54, -1, 43, 1, -1, -1, 77, -1, -1, -1, 3];
+        assert.deepStrictEqual(solution(a), [1, 3, -1, 23, 43, -1, -1, 54, -1, -1, -1, 77]);
+        console.log('Test 6: ', solution(a));
+    } catch (e) {
+        console.log(e.name, e.message);
+    }
 }
 
 testSolution();
